7413b45dc4594888ddd232a55028d7d3d164a557
Muhaimin Abdillah <muhaiminabdillah85@gmail.com>
2023-12-21 12:12:41 +0700

    simulated freezing app

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # course-dicoding-fundamental-android-app
-Project on Course [Learn Fundamental Android Development](https://www.dicoding.com/academies/14) at Dicoding Indonesia x [IDCAMP 2023](https://idcamp.ioh.co.id/)
+Task 8 : Project on Course [Learn Fundamental Android Development](https://www.dicoding.com/academies/14) at Dicoding Indonesia x [IDCAMP 2023](https://idcamp.ioh.co.id/)
+
+## Aims
+This codelab aims to implement the Background Process in the application you build. The hope is that this activity can provide a clear picture of how the processes in the background work.
+
+## Topics Covered
+- Background Process
---
+++ app/src/main/java/me/muhaimin/mybackgroundthread/MainActivity.kt
@@ -0,0 +1,34 @@
+package me.muhaimin.mybackgroundthread
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnStart: Button = findViewById(R.id.btn_start)
+        val tvStatus: TextView = findViewById(R.id.tv_status)
+
+        btnStart.setOnClickListener {
+            try {
+                // simulate process compressing
+                for (i in 0..10) {
+                    Thread.sleep(500)
+                    val percentage = i * 10
+
+                    if (percentage == 100) {
+                        tvStatus.setText(R.string.task_completed)
+                    } else {
+                        tvStatus.text = String.format(getString(R.string.compressing), percentage)
+                    }
+                }
+            }catch (e: InterruptedException) {
+                e.printStackTrace()
+            }
+        }
+    }
+}
