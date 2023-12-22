e48a6282b820fb1156c847ef2fcea7af51f65583
Muhaimin Abdillah <muhaiminabdillah85@gmail.com>
2023-12-22 22:24:48 +0700

    finish project

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # course-dicoding-fundamental-android-app
-Project on Course [Learn Fundamental Android Development](https://www.dicoding.com/academies/14) at Dicoding Indonesia x [IDCAMP 2023](https://idcamp.ioh.co.id/)
+Task 9 : Project on Course [Learn Fundamental Android Development](https://www.dicoding.com/academies/14) at Dicoding Indonesia x [IDCAMP 2023](https://idcamp.ioh.co.id/)
+
+### Aims
+learn to use Retrofit to display and send data to the server
+
+### Topics Covered
+- Retrofit
+- Send & Retrieve data from server
+- RecycleView.ListAdapter
---
+++ app/src/main/java/me/muhaimin/restaurantreview/ui/MainActivity.kt
@@ -0,0 +1,135 @@
+package me.muhaimin.restaurantreview.ui
+
+import android.content.Context
+import android.inputmethodservice.InputMethodService
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
+import me.muhaimin.restaurantreview.R
+import me.muhaimin.restaurantreview.data.response.CustomerReviewsItem
+import me.muhaimin.restaurantreview.data.response.PostReviewResponse
+import me.muhaimin.restaurantreview.data.response.Restaurant
+import me.muhaimin.restaurantreview.data.response.RestaurantResponse
+import me.muhaimin.restaurantreview.data.retrofit.ApiConfig
+import me.muhaimin.restaurantreview.databinding.ActivityMainBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    companion object {
+        private const val TAG = "MainActivity"
+        private const val RESTAURANT_ID = "uewq1zg2zlskfw1e867"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+
+        val layoutManager = LinearLayoutManager(this)
+        binding.rvReview.layoutManager = layoutManager
+
+        val itemDecoration = DividerItemDecoration(this, layoutManager.orientation)
+        binding.rvReview.addItemDecoration(itemDecoration)
+
+        findRestaurant()
+
+        binding.btnSend.setOnClickListener { view ->
+            postReview(binding.edReview.text.toString())
+
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(view.windowToken, 0)
+        }
+    }
+
+    private fun findRestaurant() {
+        showLoading(true)
+
+        val client = ApiConfig.getApiService().getRestaurant(RESTAURANT_ID)
+        client.enqueue(object: Callback<RestaurantResponse>{
+            override fun onResponse(
+                call: Call<RestaurantResponse>,
+                response: Response<RestaurantResponse>
+            ) {
+                showLoading(false)
+                if(response.isSuccessful) {
+                    val responseBody = response.body()
+                    if (responseBody != null) {
+                        setRestaurantData(responseBody.restaurant)
+                        setReviewData(responseBody.restaurant.customerReviews)
+                    }
+                } else {
+                    Log.e(TAG, "onFailure: ${response.message()}")
+                }
+            }
+
+            override fun onFailure(call: Call<RestaurantResponse>, t: Throwable) {
+                showLoading(false)
+                Log.e(TAG, "onFailure ${t.message}")
+            }
+
+        })
+    }
+
+    private fun setRestaurantData(restaurant: Restaurant) {
+        binding.tvTitle.text = restaurant.name
+        binding.tvDescription.text = restaurant.description
+        Glide.with(this@MainActivity)
+            .load("https://restaurant-api.dicoding.dev/images/large/${restaurant.pictureId}")
+            .into(binding.ivPicture)
+    }
+
+    private fun setReviewData(customerReviews: List<CustomerReviewsItem>) {
+        val adapter = ReviewAdapter()
+        adapter.submitList(customerReviews)
+
+        binding.rvReview.adapter = adapter
+        binding.edReview.setText("")
+    }
+
+    private fun showLoading(isLoading: Boolean) {
+        if (isLoading) {
+            binding.progressBar.visibility = View.INVISIBLE
+        } else {
+            binding.progressBar.visibility = View.GONE
+        }
+    }
+
+    private fun postReview(review: String) {
+        showLoading(true)
+
+        val client = ApiConfig.getApiService().postReview(RESTAURANT_ID, "Swift", review)
+        client.enqueue(object: Callback<PostReviewResponse> {
+            override fun onResponse(
+                call: Call<PostReviewResponse>,
+                response: Response<PostReviewResponse>
+            ) {
+                showLoading(false)
+                val responseBody = response.body()
+
+                if (response.isSuccessful && responseBody != null) {
+                    setReviewData(responseBody.customerReviews)
+                } else {
+                    Log.e(TAG, "onFailure: ${response.message()}")
+                }
+            }
+
+            override fun onFailure(call: Call<PostReviewResponse>, t: Throwable) {
+                showLoading(false)
+                Log.e(TAG, "onFailure: ${t.message}")
+            }
+
+        })
+    }
+}
